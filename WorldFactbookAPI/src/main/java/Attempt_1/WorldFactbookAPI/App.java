cd7344663af53ee3f86d629a3136312b683ecd15
manilonis <35714442+manilonis@users.noreply.github.com>
2019-02-14 12:48:43 -0500

    Seperated main form helper

--- WorldFactbookAPI/src/main/java/Attempt_1/WorldFactbookAPI/App.java
+++ WorldFactbookAPI/src/main/java/Attempt_1/WorldFactbookAPI/App.java
@@ -1,30 +1,10 @@
 package Attempt_1.WorldFactbookAPI;
 
-import java.io.File;
-import java.io.IOException;
+import parser2000.HTMLParser2000;
 
-import org.jsoup.*;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main(String[] args){
-    	File f = new File("/home/maniloni/Senior Project/World Factbook Data/2000/wfbfull/factbook/geos/aa.html");
-    	try {
-			Document d = Jsoup.parse(f, "UTF-8", "aa");
-			Elements ee = d.select("p");
-			//System.out.println(ee.html());
-			String p = ee.html();
-			String[] lines = p.split("\n");
-			System.out.println(lines.length);
-			System.out.println(lines[0]);
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-        System.out.println( "Hello World!" );
+    	HTMLParser2000.parse();
+        System.out.println("Completed");
     }
 }
---
+++ WorldFactbookAPI/src/main/java/parser2000/HTMLParser2000.java
@@ -0,0 +1,40 @@
+package parser2000;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class HTMLParser2000 {
+	public static void parse() {
+		File f = new File("/home/maniloni/Senior Project/World Factbook Data/2000/wfbfull/factbook/geos/aa.html");
+    	try {
+			Document d = Jsoup.parse(f, "UTF-8", "aa");
+			Elements ee = d.select("p");
+			//System.out.println(ee.html());
+			String p = ee.html();
+			String[] lines = p.split("\n");
+			//System.out.println(lines.length);
+			//System.out.println(lines[0]);
+			
+			ArrayList<String> titles = new ArrayList<String>();
+			
+			for(int i=0; i<lines.length; i++) {
+				String s = lines[i];
+				if (s.indexOf("</b>") >= 0){
+					titles.add(s);
+				}
+				else continue;
+			}
+			
+			System.out.println(titles.size());
+			System.out.println(titles.get(1));
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
