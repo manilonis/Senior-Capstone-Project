20080c22ce895ac75921da09dd90d25a2b13bf5a
manilonis <35714442+manilonis@users.noreply.github.com>
2019-02-16 16:47:36 -0500

    Reset commit

---
+++ WorldFactbookAPI/WorldFactbookAPI/src/main/java/Attempt_1/WorldFactbookAPI/App.java
@@ -0,0 +1,10 @@
+package Attempt_1.WorldFactbookAPI;
+
+import parser2000.HTMLParser2000;
+
+public class App {
+    public static void main(String[] args){
+    	HTMLParser2000.parse();
+        System.out.println("Completed");
+    }
+}
