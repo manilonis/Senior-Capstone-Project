b53102bc2020b3fb434cf9f32c619fe92bf5a7ff
manilonis <35714442+manilonis@users.noreply.github.com>
2019-02-17 15:03:04 -0500

    Check extentions

--- WorldFactbookAPI/src/main/java/parser2000/HTMLParser2000.java
+++ WorldFactbookAPI/src/main/java/parser2000/HTMLParser2000.java
@@ -24,6 +24,7 @@ public class HTMLParser2000 {
 		for (File f : files) {
 			try {
 				System.out.println(f.getName());
+				if(!f.getName().endsWith(".html")) continue;
 				Document d = Jsoup.parse(f, "UTF-8", f.getName());
 				Elements ee = d.select("p");
 				Elements top = d.select("b");
