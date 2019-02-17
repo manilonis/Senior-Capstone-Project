fe1436bb4cc61b61104b95fbfab0bc90d17a7374
manilonis <35714442+manilonis@users.noreply.github.com>
2019-02-17 16:35:14 -0500

    Run 2002

--- WorldFactbookAPI/src/main/java/Attempt_1/WorldFactbookAPI/App.java
+++ WorldFactbookAPI/src/main/java/Attempt_1/WorldFactbookAPI/App.java
@@ -1,10 +1,12 @@
 package Attempt_1.WorldFactbookAPI;
 
 import parsers.HTMLParser2000;
+import parsers.HTMLParser2002;
 
 public class App {
     public static void main(String[] args){
     	HTMLParser2000.parse();
+    	HTMLParser2002.parse();
         System.out.println("Completed");
     }
 }
