ae4ef2ab9efb26cf6e9daabe33dda1c525d3bcb9
manilonis <35714442+manilonis@users.noreply.github.com>
2019-02-21 16:06:02 -0500

    One year at a time

--- WorldFactbookAPI/src/main/java/Attempt_1/WorldFactbookAPI/App.java
+++ WorldFactbookAPI/src/main/java/Attempt_1/WorldFactbookAPI/App.java
@@ -7,11 +7,13 @@ import parsers.HTMLParser2002;
 public class App {
     public static void main(String[] args){
     	HTMLParser2000.parse();
+    	System.out.println("2002 going");
     	Early2000sParser.parse("2002", "/home/maniloni/Senior Project/World Factbook Data/2002/geos/");
+    	System.out.println("2003 going");
     	Early2000sParser.parse("2003", "/home/maniloni/Senior Project/World Factbook Data/2003/geos/");
-    	Early2000sParser.parse("2004", "/home/maniloni/Senior Project/World Factbook Data/2004/factbook/geos/");
+    	/*Early2000sParser.parse("2004", "/home/maniloni/Senior Project/World Factbook Data/2004/factbook/geos/");
     	Early2000sParser.parse("2005", "/home/maniloni/Senior Project/World Factbook Data/2005/factbook/geos/");
-    	Early2000sParser.parse("2006", "/home/maniloni/Senior Project/World Factbook Data/2006/factbook/geos/");
+    	Early2000sParser.parse("2006", "/home/maniloni/Senior Project/World Factbook Data/2006/factbook/geos/");*/
     	//HTMLParser2002.parse();
         System.out.println("Completed");
     }
