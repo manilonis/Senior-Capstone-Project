428bdce68bcbca9abaa267bf05e7223ea3de9145
manilonis <35714442+manilonis@users.noreply.github.com>
2019-02-21 17:15:12 -0500

    Skipping EU data

--- WorldFactbookAPI/src/main/java/parsers/Early2000sParser.java
+++ WorldFactbookAPI/src/main/java/parsers/Early2000sParser.java
@@ -96,15 +96,8 @@ public class Early2000sParser {
 			int c = str.lastIndexOf("</a>");
 			if (c >= 0) {
 				String temp = str.substring(c + 4);
-				if (temp.contains("</font>") || temp.contains("</div>") || temp.contains("</p>") || temp.contains("</span>"))
+				if (temp.contains("</font>") || temp.contains("</div>") || temp.contains("</p>") || temp.contains("</span>") || d.baseUri().equals("ee.html"))
 					continue;
-				if (d.baseUri().equals("ee.html")) {
-					if(temp.contains("Following the two")) {
-						if(!data.contains(temp))data.add(temp);
-						//System.out.println("EXCEPTION HIT: " + temp);
-						continue;
-					}
-				}
 				if (temp.lastIndexOf("<br>") >= 0)
 					data.add(temp.substring(temp.lastIndexOf("<br>") + 4).trim());
 				else
@@ -117,14 +110,9 @@ public class Early2000sParser {
 		if (topics.size() != data.size()) {
 			System.out.println("Error at " + d.baseUri());
 			System.out.println(topics.toString());
-			//System.out.println(data.toString());
+			System.out.println(data.toString());
 		}
 		for (int i = 0; i < topics.size(); i++) {
-			//DEBUG 
-			if(i >= data.size()){
-				System.out.println("EXTRA HEADER IS: " + topics.get(i));
-				continue;
-			}
 			allData.put(topics.get(i), data.get(i));
 		}
 		if(topics.size() != data.size() || d.baseUri().equals("aa.html")) {
