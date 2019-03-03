b26c59285eb617e86113e9dc5b0f0ec7e096a61f
manilonis <35714442+manilonis@users.noreply.github.com>
2019-03-03 16:01:05 -0500

    Different debug statement

--- WorldFactbookAPI/src/main/java/parsers/Early2000sParser.java
+++ WorldFactbookAPI/src/main/java/parsers/Early2000sParser.java
@@ -120,7 +120,8 @@ public class Early2000sParser {
 		}
 		System.out.println("Size of new lists: headers: " + headers.size()+" data: " + data.size());
 		for(int i =0; i< headers.size(); i++) {
-			System.out.println("Header: " + headers.get(i) + " data: "+ data.get(i));
+			//System.out.println("Header: " + headers.get(i) + " data: "+ data.get(i));
+			System.out.println("Data " + i + ": " + data.get(i));
 		}
 	
 	}
