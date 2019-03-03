c1578f01a2b66aa4a01542bf4dcd317784bfc50c
manilonis <35714442+manilonis@users.noreply.github.com>
2019-03-03 15:42:11 -0500

    Going year by year

--- WorldFactbookAPI/src/main/java/Attempt_1/WorldFactbookAPI/App.java
+++ WorldFactbookAPI/src/main/java/Attempt_1/WorldFactbookAPI/App.java
@@ -9,9 +9,9 @@ public class App {
     	HTMLParser2000.parse();
     	System.out.println("2002 going");
     	Early2000sParser.parse("2002", "/home/maniloni/Senior Project/World Factbook Data/2002/geos/");
-    	/*System.out.println("2003 going");
+    	System.out.println("2003 going");
     	Early2000sParser.parse("2003", "/home/maniloni/Senior Project/World Factbook Data/2003/geos/");
-    	System.out.println("2004 going");
+    	/*System.out.println("2004 going");
     	Early2000sParser.parse("2004", "/home/maniloni/Senior Project/World Factbook Data/2004/factbook/geos/");
     	System.out.println("2005 going");
     	Early2000sParser.parse("2005", "/home/maniloni/Senior Project/World Factbook Data/2005/factbook/geos/");*/
