b9a656debad7f96462f2b90e7af0a1d324dcff68
manilonis <35714442+manilonis@users.noreply.github.com>
2019-03-04 10:25:28 -0500

    Look at 2004

--- WorldFactbookAPI/src/main/java/Attempt_1/WorldFactbookAPI/App.java
+++ WorldFactbookAPI/src/main/java/Attempt_1/WorldFactbookAPI/App.java
@@ -11,13 +11,12 @@ public class App {
     	Early2000sParser.parse("2002", "/home/maniloni/Senior Project/World Factbook Data/2002/geos/");
     	System.out.println("2003 going");
     	Early2000sParser.parse("2003", "/home/maniloni/Senior Project/World Factbook Data/2003/geos/");*/
-    	//System.out.println("2004 going");
-    	//Early2000sParser.parse("2004", "/home/maniloni/Senior Project/World Factbook Data/2004/factbook/geos/");
+    	System.out.println("2004 going");
+    	Early2000sParser.parse("2004", "/home/maniloni/Senior Project/World Factbook Data/2004/factbook/geos/");
     	System.out.println("2005 going");
     	Early2000sParser.parse("2005", "/home/maniloni/Senior Project/World Factbook Data/2005/factbook/geos/");
     	System.out.println("2006 going");
     	Early2000sParser.parse("2006", "/home/maniloni/Senior Project/World Factbook Data/2006/factbook/geos/");
-    	//HTMLParser2002.parse();
         System.out.println("Completed");
     }
 }
