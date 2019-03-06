dc59663d864f4059a35466377c1dc0ce43829efe
manilonis <manilonis741@gmail.com>
2019-03-06 11:04:22 -0500

    Grabbing random data from the API

--- MachineLearningModel/APICall.py
+++ MachineLearningModel/APICall.py
@@ -3,15 +3,17 @@ import sys
 import random
 
 if __name__ == "__main__":
-    resp = requests.get('http://pi.cs.oswego.edu:4567/2002')
+    resp = requests.get('http://pi.cs.oswego.edu:4567/2004')
     if resp.status_code != 200:
         print("Something went wrong")
         sys.exit(5)
     json_response = resp.json()
     countries = json_response["countries"]
-    new_resp = requests.get('http://pi.cs.oswego.edu:4567/2002/' + countries[random.randint(0, len(countries))])
+    new_resp = requests.get('http://pi.cs.oswego.edu:4567/2004/' + countries[random.randint(0, len(countries))])
     new_json = new_resp.json()
     print(new_resp.status_code)
-    print(new_json["Country"])
+    keys = list(new_json["data"].keys())
+    print(keys)
+    print(new_json["data"][keys[random.randint(0, len(keys))]])
     # print(len(json_response["countries"]))
     # print(resp.json())
