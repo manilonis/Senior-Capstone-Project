3d74c9471f76ee8b6cbeb7c4bee7f6a29da1874b
manilonis <35714442+manilonis@users.noreply.github.com>
2019-03-07 16:28:41 -0500

    Now touching the right file

--- WorldFactbookAPI/src/main/java/parsers/HTMLParser2000.java
+++ WorldFactbookAPI/src/main/java/parsers/HTMLParser2000.java
@@ -42,13 +42,6 @@ public class HTMLParser2000 {
 				HashMap<String, String> data;
 				System.out.println(f.getName());
 				if(!f.getName().endsWith(".html")) continue;
-				if(f.getName().contains("_") || f.getName().contains("countrytemplate")) {
-					System.out.println("got template");
-					continue;
-				}
-				else {
-					System.out.println(f.getName());
-				}
 				Document d = Jsoup.parse(f, "UTF-8", f.getName());
 				Elements ee = d.select("p");
 				Elements top = d.select("b");
