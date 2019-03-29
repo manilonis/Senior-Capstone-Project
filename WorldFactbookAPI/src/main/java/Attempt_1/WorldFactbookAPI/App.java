b5770a139675372c819fbb9dc9ff013866302c21
manilonis <35714442+manilonis@users.noreply.github.com>
2019-03-29 14:58:33 -0400

    Format all code

--- WorldFactbookAPI/src/main/java/Attempt_1/WorldFactbookAPI/App.java
+++ WorldFactbookAPI/src/main/java/Attempt_1/WorldFactbookAPI/App.java
@@ -5,32 +5,32 @@ import parsers.HTMLParser2000;
 import parsers.NewParser;
 
 public class App {
-    public static void main(String[] args){
-    	System.out.println("2000 going");
-    	HTMLParser2000.parse();
-    	System.out.println("2002 going");
-    	Early2000sParser.parse("2002", "/home/maniloni/Senior Project/World Factbook Data/2002/geos/");
-    	System.out.println("2003 going");
-    	Early2000sParser.parse("2003", "/home/maniloni/Senior Project/World Factbook Data/2003/geos/");
-    	System.out.println("2004 going");
-    	Early2000sParser.parse("2004", "/home/maniloni/Senior Project/World Factbook Data/2004/factbook/geos/");
-    	System.out.println("2005 going");
-    	Early2000sParser.parse("2005", "/home/maniloni/Senior Project/World Factbook Data/2005/factbook/geos/");
-    	System.out.println("2006 going");
-    	Early2000sParser.parse("2006", "/home/maniloni/Senior Project/World Factbook Data/2006/factbook/geos/");
-    	System.out.println("2007 going");
-    	NewParser.parse("2007","/home/maniloni/Senior Project/World Factbook Data/2007/factbook/geos/");
-    	System.out.println("2008 going");
-    	NewParser.parse("2008", "/home/maniloni/Senior Project/World Factbook Data/2008/factbook/geos/");
-    	System.out.println("2009 going");
-    	NewParser.parse("2009", "/home/maniloni/Senior Project/World Factbook Data/2009/geos/");
-    	System.out.println("2010 going");
-    	NewParser.parse("2010", "/home/maniloni/Senior Project/World Factbook Data/2010/factbook/geos/");
-    	System.out.println("2011 going");
-    	NewParser.parse("2011", "/home/maniloni/Senior Project/World Factbook Data/2011/factbook/geos/" );
-    	System.out.println("2012 going");
-    	NewParser.parse("2012", "/home/maniloni/Senior Project/World Factbook Data/2012/factbook/geos/");
-        System.out.println("Completed");
-       
-    }
+	public static void main(String[] args) {
+		System.out.println("2000 going");
+		HTMLParser2000.parse();
+		System.out.println("2002 going");
+		Early2000sParser.parse("2002", "/home/maniloni/Senior Project/World Factbook Data/2002/geos/");
+		System.out.println("2003 going");
+		Early2000sParser.parse("2003", "/home/maniloni/Senior Project/World Factbook Data/2003/geos/");
+		System.out.println("2004 going");
+		Early2000sParser.parse("2004", "/home/maniloni/Senior Project/World Factbook Data/2004/factbook/geos/");
+		System.out.println("2005 going");
+		Early2000sParser.parse("2005", "/home/maniloni/Senior Project/World Factbook Data/2005/factbook/geos/");
+		System.out.println("2006 going");
+		Early2000sParser.parse("2006", "/home/maniloni/Senior Project/World Factbook Data/2006/factbook/geos/");
+		System.out.println("2007 going");
+		NewParser.parse("2007", "/home/maniloni/Senior Project/World Factbook Data/2007/factbook/geos/");
+		System.out.println("2008 going");
+		NewParser.parse("2008", "/home/maniloni/Senior Project/World Factbook Data/2008/factbook/geos/");
+		System.out.println("2009 going");
+		NewParser.parse("2009", "/home/maniloni/Senior Project/World Factbook Data/2009/geos/");
+		System.out.println("2010 going");
+		NewParser.parse("2010", "/home/maniloni/Senior Project/World Factbook Data/2010/factbook/geos/");
+		System.out.println("2011 going");
+		NewParser.parse("2011", "/home/maniloni/Senior Project/World Factbook Data/2011/factbook/geos/");
+		System.out.println("2012 going");
+		NewParser.parse("2012", "/home/maniloni/Senior Project/World Factbook Data/2012/factbook/geos/");
+		System.out.println("Completed");
+
+	}
 }
