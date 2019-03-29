05ec2dec88e94d24a8effce93cbf441e5d049d9e
manilonis <35714442+manilonis@users.noreply.github.com>
2019-03-29 14:34:10 -0400

    Try bulk 2009 parse

--- WorldFactbookAPI/src/main/java/Attempt_1/WorldFactbookAPI/App.java
+++ WorldFactbookAPI/src/main/java/Attempt_1/WorldFactbookAPI/App.java
@@ -18,16 +18,12 @@ public class App {
     	Early2000sParser.parse("2005", "/home/maniloni/Senior Project/World Factbook Data/2005/factbook/geos/");
     	System.out.println("2006 going");
     	Early2000sParser.parse("2006", "/home/maniloni/Senior Project/World Factbook Data/2006/factbook/geos/");
-//    	System.out.println("2007 going");
-//    	Early2000sParser.parse("2007", "/home/maniloni/Senior Project/World Factbook Data/2007/factbook/geos/");
-//    	System.out.println("2009 going");
-//    	Early2000sParser.parse("2009", "/home/maniloni/Senior Project/World Factbook Data/2009/geos/");
     	System.out.println("2007 going");
     	NewParser.parse("2007","/home/maniloni/Senior Project/World Factbook Data/2007/factbook/geos/");
     	System.out.println("2008 going");
     	NewParser.parse("2008", "/home/maniloni/Senior Project/World Factbook Data/2008/factbook/geos/");
     	System.out.println("2009 going");
-    	NewParser.testMethodforNewYear("2009", "/home/maniloni/Senior Project/World Factbook Data/2009/geos/");
+    	NewParser.parse("2009", "/home/maniloni/Senior Project/World Factbook Data/2009/geos/");
         System.out.println("Completed");
        
     }
