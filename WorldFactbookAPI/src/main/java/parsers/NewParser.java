aa8f80c79187e2722633bf79d078a7a5be3f4ca8
manilonis <35714442+manilonis@users.noreply.github.com>
2019-03-29 13:04:20 -0400

    Look at ascii of empty string

--- WorldFactbookAPI/src/main/java/parsers/NewParser.java
+++ WorldFactbookAPI/src/main/java/parsers/NewParser.java
@@ -37,7 +37,7 @@ public class NewParser {
 				texts_trimmed.add(t);
 			}
 			System.out.println(texts_trimmed);
-			System.out.println(texts_trimmed.get(texts_trimmed.size()-1).length());
+			System.out.println((int)(texts_trimmed.get(texts_trimmed.size()-7).toCharArray()[0]));
 			//System.out.println(Arrays.toString(text.split("\n")));
 			char[] text_array = text.toCharArray();
 			ArrayList<Integer> indicies = new ArrayList<Integer>();
