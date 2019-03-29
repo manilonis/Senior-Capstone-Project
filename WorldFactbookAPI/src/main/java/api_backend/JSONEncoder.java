51e928eea9b868db561b598b6c750a6dda63a304
manilonis <35714442+manilonis@users.noreply.github.com>
2019-03-29 15:09:50 -0400

    Format code

--- WorldFactbookAPI/src/main/java/api_backend/JSONEncoder.java
+++ WorldFactbookAPI/src/main/java/api_backend/JSONEncoder.java
@@ -11,39 +11,40 @@ import java.util.Set;
 import org.json.JSONObject;
 
 public class JSONEncoder {
-	
-	public static JSONObject encodeYear(String year, HashMap<String, HashMap<String, String>> yearMap, HashMap<String, String> reverseCountryCode) {
+
+	public static JSONObject encodeYear(String year, HashMap<String, HashMap<String, String>> yearMap,
+			HashMap<String, String> reverseCountryCode) {
 		JSONObject r = new JSONObject();
 		Set<String> keys = yearMap.keySet();
 		ArrayList<String> countryNames = new ArrayList<String>();
-		for(String str: keys) {
+		for (String str : keys) {
 			str = str.replace(".html", "");
 			String country = reverseCountryCode.get(str);
-			if(country == null) {
-				System.out.println("Error with "+str);
+			if (country == null) {
+				System.out.println("Error with " + str);
 				continue;
 			}
 			countryNames.add(reverseCountryCode.get(str));
 		}
-		
+
 		r.put("year", year);
 		r.put("countries", countryNames.toArray());
 		return r;
 	}
-	
+
 	public static JSONObject encodeCountry(String name, HashMap<String, String> country) {
 		System.out.println(country.size());
 		Set<String> keys = country.keySet();
 		JSONObject r = new JSONObject();
 		r.put("Country", name);
 		JSONObject countryData = new JSONObject();
-		for(String str: keys) {
+		for (String str : keys) {
 			String data = country.get(str);
 			countryData.put(str, data);
 		}
-		
+
 		r.put("data", countryData);
-		
+
 		return r;
 	}
 }
--- WorldFactbookAPI/src/main/java/api_backend/MyAPI.java
+++ WorldFactbookAPI/src/main/java/api_backend/MyAPI.java
@@ -20,91 +20,93 @@ public class MyAPI {
 	public static void main(String[] args) {
 		HashMap<String, String> countryDict = getCountryNames();
 		HashMap<String, String> reverseDict = new HashMap<String, String>();
-		for(Map.Entry<String, String> entry : countryDict.entrySet()){
-		    reverseDict.put(entry.getValue(), entry.getKey());
+		for (Map.Entry<String, String> entry : countryDict.entrySet()) {
+			reverseDict.put(entry.getValue(), entry.getKey());
 		}
 		File[] serialized = new File("/home/maniloni/Senior Project/serialized_data/").listFiles();
 		String[] years = new String[serialized.length];
-		for(int i=0; i<serialized.length; i++) {
+		for (int i = 0; i < serialized.length; i++) {
 			String file = serialized[i].getName();
 			years[i] = file.substring(0, file.indexOf(".ser"));
 		}
 		System.out.println(Arrays.toString(years));
+
 		@SuppressWarnings("unchecked")
-		HashMap<String, HashMap<String, String>>[] maps = new HashMap [years.length];
-		for(int i=0; i<years.length; i++) {
+		HashMap<String, HashMap<String, String>>[] maps = new HashMap[years.length];
+		for (int i = 0; i < years.length; i++) {
 			maps[i] = loadFile(years[i]);
 		}
-		get("/hello", (req, res)->"Hello, world");
-		
-		get("/:year", (req,res)->{
+		get("/hello", (req, res) -> "Hello, world");
+
+		get("/:year", (req, res) -> {
 			String year = req.params("year");
 			HashMap<String, HashMap<String, String>> thisYear = maps[getYearfromArray(year, years)];
 			return JSONEncoder.encodeYear(year, thisYear, reverseDict).toString(4);
 		});
-		
-		get("/:year/:country", (req, res)->{
+
+		get("/:year/:country", (req, res) -> {
 			String year = req.params("year");
 			String country = req.params("country");
 			String file;
-			if(country.length() == 2) file=country.toLowerCase()+".html";
+			if (country.length() == 2)
+				file = country.toLowerCase() + ".html";
 			else {
 				file = countryDict.get(country) + ".html";
 			}
 			HashMap<String, HashMap<String, String>> thisYear = maps[getYearfromArray(year, years)];
-			
-			if(thisYear.get(file) == null) {
+
+			if (thisYear.get(file) == null) {
 				return "Error: Country does not exist";
 			}
-			
+
 			return JSONEncoder.encodeCountry(country, thisYear.get(file)).toString(4);
 		});
-		
-		
+
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	private static HashMap<String, HashMap<String, String>> loadFile(String year){
+	private static HashMap<String, HashMap<String, String>> loadFile(String year) {
 		try {
-			FileInputStream fin = new FileInputStream("/home/maniloni/Senior Project/serialized_data/"+year+".ser");
+			FileInputStream fin = new FileInputStream("/home/maniloni/Senior Project/serialized_data/" + year + ".ser");
 			ObjectInputStream oin = new ObjectInputStream(fin);
 			HashMap<String, HashMap<String, String>> r = (HashMap<String, HashMap<String, String>>) oin.readObject();
 			oin.close();
 			fin.close();
 			return r;
-		}catch(IOException io) {
+		} catch (IOException io) {
 			io.printStackTrace();
-		} catch(ClassNotFoundException ce) {
+		} catch (ClassNotFoundException ce) {
 			ce.printStackTrace();
 		}
 		System.out.println("LOAD ERROR " + year);
 		return null;
 	}
-	
+
 	private static int getYearfromArray(String year, String[] years) {
-		for(int i=0; i<years.length; i++) {
-			if(year.equals(years[i])) return i;
+		for (int i = 0; i < years.length; i++) {
+			if (year.equals(years[i]))
+				return i;
 		}
 		return -1;
 	}
-	
-	private static HashMap<String, String> getCountryNames(){
+
+	private static HashMap<String, String> getCountryNames() {
 		HashMap<String, String> names = new HashMap<String, String>();
 		File f = new File("/home/maniloni/Senior Project/World Factbook Data/country codes.csv");
 		try {
 			Scanner s = new Scanner(f);
 			s.useDelimiter(",|\n");
-			while(s.hasNext()) {
+			while (s.hasNext()) {
 				String wholeName = s.next();
 				String code = s.next();
-				if(!code.contains("-"))
-				names.put(wholeName.trim(), code.toLowerCase().trim());
+				if (!code.contains("-"))
+					names.put(wholeName.trim(), code.toLowerCase().trim());
 			}
 			s.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		return names;
-		
+
 	}
 }
